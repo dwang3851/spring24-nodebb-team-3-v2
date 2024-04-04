9cf89b16e836feaf2b28f5be6c451983297bb853
KevinHeeHee <KevinHeeHee@users.noreply.github.com>
2024-04-04 07:01:50 +0000

    Prettified Code!

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,14 @@
-var request = require('request')
+var request = require("request");
 
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    const response = await fetch(
+        process.env.TRANSLATOR_API + "/?content=" + postData.content,
+    );
     const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+    return [data["is_english"], data["translated_content"]];
+};
